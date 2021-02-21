ff16706c91bb7f44a59d070cef602685ba780184
smooth-dasilva <luan.dasilva@smoothstack.com>
2021-02-21 17:41:26 -0500

    Commits placeholders

---
+++ ExpediaReportingApp/app.py
@@ -0,0 +1,13 @@
+#main
+
+def main():
+    print("main...")
+
+if __name__ == "__main__":
+    main()
+
+class TestUnitTests:
+    test : str
+    
+    def __init__(self) -> None:
+        test = "test works..."
---
+++ ExpediaReportingApp/testapp.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class Tests(unittest.TestCase):
+    def unitTestTest(self):
+        self.assertEqual("test works...","test works...")
+
+
+#run actual ut
+unittest.main()
