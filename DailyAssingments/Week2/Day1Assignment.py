32bc6907fdd810ee4d66a7b882598d0f8c1b4c23
smooth-dasilva <luan.dasilva@smoothstack.com>
2021-02-24 16:44:52 -0500

    Adds Week2 Day1 Exercises

---
+++ DailyAssingments/Week2/Day1Assignment.py
@@ -0,0 +1,19 @@
+#1
+import numpy as np
+#2
+print(np.zeros(10))
+#3
+print(np.ones(10))
+#4
+print(list(map(lambda OnesToFive: OnesToFive*5 , np.ones(10))))
+#5
+print(np.arange(10,51))
+#6
+print(list(filter( lambda OnlyEvens : OnlyEvens%2==0,np.arange(10,51))))
+#7
+
+#8
+print(np.random.randint(0, 8, (3,3) ))
+#9
+print(np.random.rand(1))
+
